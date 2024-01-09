7ecd9406f2c27642a3c930a5bba1b281402cec03
Yevhenii Saveliev <88836913+saveliyevyevhenii@users.noreply.github.com>
2024-01-09 17:47:46 +0200

    Create GameResult.cs

---
+++ TicTacToe/GameResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    public class GameResult
+    {
+        public Player Winner { get; set; }
+
+        public WinInfo WinInfo { get; set; }
+    }
+}

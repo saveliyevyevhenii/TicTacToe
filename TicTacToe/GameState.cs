5673c5dad09f5b80192ab338388b8563467b3e5e
Yevhenii Saveliev <88836913+saveliyevyevhenii@users.noreply.github.com>
2024-01-09 23:00:42 +0200

    Create GameState.cs

---
+++ TicTacToe/GameState.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    public class GameState
+    {
+        public Player[,] GameGrid { get; private set; }
+
+        public Player CurrentPlayer { get; private set; }
+
+        public int TurnsPassed { get; private set; }
+
+        public bool GameOver { get; private set; }
+
+        public event Action<int, int> MoveMade;
+
+        public event Action<GameResult> GameEnded;
+
+        public event Action GameRestarted;
+
+        public GameState()
+        {
+            GameGrid = new Player[3,3];
+            CurrentPlayer = Player.X;
+            TurnsPassed = 0;
+            GameOver = false;
+        }
+
+
+    }
+}
